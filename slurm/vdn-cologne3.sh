b2c4bece252fba55b68405e9e770796b39a7cb3d
Guilherme Varela <guilhermevarela@protonmail.com>
2025-03-15 07:37:14 +0000

    Add vdn ns agents configuration and script

---
+++ slurm/vdn-cologne3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=CologneCorridorVDN
+#SBATCH --output=/home/u021427/logs/RESCO/cologne_corridor_vdn_%A.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8GB
+#SBATCH --time=16:00:00
+
+## Activate pyenv
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv activate RESCO
+
+
+## Export
+export LIBSUMO_AS_TRACI=1
+
+## Init job
+ALGO=vdn_ns
+MAP=cologne3
+TASK=resco_benchmark:"${MAP}"-"${ALGO}"-v1
+for i in {0..4}
+do
+   python src/main.py --config="${ALGO}" --env-config=resco with env_args.key="${TASK}" env_args.tr="$i" --force
+   echo "Running with ${ALGO} and ${TASK} trace $i"
+   sleep 2s
+done
