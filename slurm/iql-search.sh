df4ea3d4857ca5a73114ac6bf983221d4fff2b60
Guilherme Varela <guilhermevarela@protonmail.com>
2025-03-15 12:12:29 +0000

    Add iql ns agents search script

---
+++ slurm/iql-search.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=CologneCorridorSearchIQL
+#SBATCH --output=/home/u021427/logs/RESCO/search_iql_%A.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=25
+#SBATCH --mem=80GB
+#SBATCH --time=48:00:00
+
+## Activate pyenv
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv activate RESCO
+
+
+## Export
+unset LIBSUMO_AS_TRACI=1
+export LIBTRACI_AS_TRACI=1
+
+## Change to src
+cd src
+
+## Init job
+ALGO=iql_ns
+CPUS=20
+SEEDS=3
+python search.py run --config=search.config.iql_ns.yaml --seeds "${SEEDS}" locally --cpus "${CPUS}"
--- src/search.py
+++ src/search.py
@@ -108,13 +108,18 @@ def run(ctx, config, shuffle, seeds):
 )
 @click.pass_obj
 def locally(combos, cpus):
-    configs = ["python main.py " + " ".join([c for c in combo if c.startswith("--")]) + " with " + " ".join([c for c in combo if not c.startswith("--")]) for combo in combos]
+    configs = [
+        "python main.py "
+        + " ".join([c for c in combo if c.startswith("--")])
+        + " with "
+        + " ".join([c for c in combo if not c.startswith("--")])
+        for combo in combos
+    ]
 
-    click.confirm(
-        f"There are {click.style(str(len(combos)), fg='red')} combinations of configurations. Up to {cpus} will run in parallel. Continue?",
-        abort=True,
-    )
-
+    # click.confirm(
+    #     f"There are {click.style(str(len(combos)), fg='red')} combinations of configurations. Up to {cpus} will run in parallel. Continue?",
+    #     abort=True,
+    # )
 
     pool = multiprocessing.Pool(processes=cpus)
     print(pool.map(work, configs))
@@ -122,7 +127,8 @@ def locally(combos, cpus):
 
 @run.command()
 @click.argument(
-    "index", type=int,
+    "index",
+    type=int,
 )
 @click.pass_obj
 def single(combos, index):
@@ -131,7 +137,12 @@ def single(combos, index):
     """
 
     config = combos[index]
-    cmd = "python main.py " + " ".join([c for c in config if c.startswith("--")]) + " with " + " ".join([c for c in config if not c.startswith("--")])
+    cmd = (
+        "python main.py "
+        + " ".join([c for c in config if c.startswith("--")])
+        + " with "
+        + " ".join([c for c in config if not c.startswith("--")])
+    )
     print(cmd)
     work(cmd)
 
