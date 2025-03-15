dd76640c1b433ef1f9fdc3714a78e806fff4f9da
Guilherme Varela <guilhermevarela@protonmail.com>
2025-03-15 14:04:19 +0000

    Add dvdn ns agent scripts

---
+++ slurm/dvdn-search.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=CologneCorridorSearchDVDN
+#SBATCH --output=/home/u021427/logs/RESCO/search_dvdn_%A.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=120GB
+#SBATCH --time=96:00:00
+
+## Activate pyenv
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv activate RESCO
+
+
+## Export
+unset LIBSUMO_AS_TRACI=1
+export LIBTRACI_AS_TRACI=1
+
+## Change to src
+cd src
+
+## Init job
+ALGO=dvdn_ns
+CPUS=32
+SEEDS=3
+python search.py run --config=search.config.dvdn_ns.yaml --seeds "${SEEDS}" locally --cpus "${CPUS}"
