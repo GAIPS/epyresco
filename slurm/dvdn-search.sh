446fa702d1afcdf0ed909d7a3d730888a285a147
Guilherme Varela <guilhermevarela@protonmail.com>
2025-03-24 16:43:53 +0000

    Update dvdn ns agents search script

--- slurm/dvdn-search.sh
+++ slurm/dvdn-search.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=/home/u021427/logs/RESCO/search_dvdn_%A.out
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=45
 #SBATCH --mem=120GB
 #SBATCH --time=96:00:00
 
@@ -22,6 +22,6 @@ cd src
 
 ## Init job
 ALGO=dvdn_ns
-CPUS=32
+CPUS=40
 SEEDS=3
 python search.py run --config=search.config.dvdn_ns.yaml --seeds "${SEEDS}" locally --cpus "${CPUS}"
