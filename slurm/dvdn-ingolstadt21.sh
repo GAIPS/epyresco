211aab2a973a34fedc232d481d74ce47d242edda
Guilherme Varela <guilhermevarela@protonmail.com>
2025-04-11 12:28:23 +0100

    Increase t_max for ingolstaldt21 to 864_000 time steps.
    Training stopped before the systems were converging.

--- slurm/dvdn-ingolstadt21.sh
+++ slurm/dvdn-ingolstadt21.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=150GB
-#SBATCH --time=36:00:00
+#SBATCH --time=48:00:00
 
 ## Activate pyenv
 eval "$(pyenv init -)"
@@ -23,7 +23,7 @@ cd src
 ## Init job
 ALGO=dvdn_ns
 MAP=ingolstadt21
-CPUS=5
+CPUS=1
 SEEDS=1
 TASK=resco_benchmark:"${MAP}"-"${ALGO}"-v1
 python train.py run --config=train.config.dvdn_ns.yaml --seeds "${SEEDS}" locally --cpus "${CPUS}"
