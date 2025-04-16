7f34ca2fae79b6454dfb88293520f70e3212133d
Guilherme Varela <guilhermevarela@protonmail.com>
2025-04-16 15:41:17 +0100

    Add Lisbon 6 train slurm scritps

---
+++ slurm/iql-lisbon6.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=LisbonRegionSmallIQL
+#SBATCH --output=/home/u021427/logs/RESCO/lisbon_region_small_iql_%A.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8GB
+#SBATCH --time=12:00:00
+
+## Activate pyenv
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv activate RESCO
+
+
+## Export
+export LIBSUMO_AS_TRACI=1
+
+## Init job
+ALGO=iql_ns
+MAP=lisbon6
+TASK=resco_benchmark:"${MAP}"-"${ALGO}"-v1
+
+for i in {0..4}
+do
+   python src/main.py --config="${ALGO}" --env-config=resco with env_args.key="${TASK}" env_args.tr="$i" --force
+   echo "Running with ${ALGO} and ${TASK} trace $i"
+   sleep 2s
+done
