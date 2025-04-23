5051cb6a219b80fe022700ecac668e3deed06c06
Guilherme <guilhermevarela@protonmail.com>
2025-04-23 11:01:44 +0100

    Update search scripts for slurm

--- slurm/dvdn-search.sh
+++ slurm/dvdn-search.sh
@@ -3,8 +3,8 @@
 #SBATCH --output=/home/u021427/logs/RESCO/search_dvdn_%A.out
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=45
-#SBATCH --mem=120GB
+#SBATCH --cpus-per-task=35
+#SBATCH --mem=350GB
 #SBATCH --time=96:00:00
 
 ## Activate pyenv
@@ -14,14 +14,14 @@ pyenv activate RESCO
 
 
 ## Export
-unset LIBSUMO_AS_TRACI=1
-export LIBTRACI_AS_TRACI=1
+unset LIBTRACI_AS_TRACI
+export LIBSUMO_AS_TRACI=1
 
 ## Change to src
 cd src
 
 ## Init job
 ALGO=dvdn_ns
-CPUS=40
+CPUS=35
 SEEDS=3
 python search.py run --config=search.config.dvdn_ns.yaml --seeds "${SEEDS}" locally --cpus "${CPUS}"
--- slurm/iql-search.sh
+++ slurm/iql-search.sh
@@ -3,9 +3,9 @@
 #SBATCH --output=/home/u021427/logs/RESCO/search_iql_%A.out
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=45
-#SBATCH --mem=80GB
-#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=25
+#SBATCH --mem=240GB
+#SBATCH --time=72:00:00
 
 ## Activate pyenv
 eval "$(pyenv init -)"
@@ -14,14 +14,14 @@ pyenv activate RESCO
 
 
 ## Export
-unset LIBSUMO_AS_TRACI=1
-export LIBTRACI_AS_TRACI=1
+unset LIBTRACI_AS_TRACI
+export LIBSUMO_AS_TRACI=1
 
 ## Change to src
 cd src
 
 ## Init job
 ALGO=iql_ns
-CPUS=45
+CPUS=25
 SEEDS=3
 python search.py run --config=search.config.iql_ns.yaml --seeds "${SEEDS}" locally --cpus "${CPUS}"
