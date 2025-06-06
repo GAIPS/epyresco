27416b2c07a0c5d23d06c22e08bf28bbcbfbd822
Guilherme Varela <guilhermevarela@protonmail.com>
2025-06-06 14:16:41 +0100

    Add centralV for multiagent advantage actor critic

--- src/controllers/non_shared_controller.py
+++ src/controllers/non_shared_controller.py
@@ -1,6 +1,7 @@
 from modules.agents import REGISTRY as agent_REGISTRY
 from components.action_selectors import REGISTRY as action_REGISTRY
 import torch as th
+from IPython.core.debugger import set_trace
 
 
 class NonSharedMAC:
@@ -39,7 +40,7 @@ class NonSharedMAC:
                 # reshaped_avail_actions = avail_actions.reshape(
                 #     ep_batch.batch_size * self.n_agents, -1
                 # )
-                agent_outs[avail_actions == 0] = -1e10
+                agent_outs[avail_actions.squeeze() == 0] = -1e10
 
             agent_outs = th.nn.functional.softmax(agent_outs, dim=-1)
         return agent_outs.view(ep_batch.batch_size, self.n_agents, -1)
